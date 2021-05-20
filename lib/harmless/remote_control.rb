46c29c7a010d959b3e65d0321fd31b3484b2fe50
Levi Bard <taktaktaktaktaktaktaktaktaktak@gmail.com>
2021-05-20 16:01:31 +0200

    Add delete message remote control function

--- lib/harmless/remote_control.rb
+++ lib/harmless/remote_control.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require 'discordrb'
 require_relative 'credentials'
 
 module Harmless
   class RemoteControl
     MSGRE = /^MSG #([-\w]+)\s+(.*)/
+    DELETERE = /^DELETE #([-\w]+)\s+(\d+)/
+
+    MESSAGE_LOOKUP_COUNT = 20
 
     def initialize(harmless, bot)
       @harmless = harmless
@@ -21,15 +25,39 @@ module Harmless
       case command
       when 'GRUEDUMP'
         @harmless.gruedump
-      when MSGRE
-        if (match = command.match(MSGRE))
-          @bot.servers.each_value do |server|
-            puts "checking server #{server} for ##{match[1]}"
-            if (channel = server.text_channels.detect { |channel| channel.name == match[1] })
-              @bot.send_message(channel.id, match[2])
-              return
+
+      when DELETERE
+        if (match = command.match(DELETERE))
+          messageIndex = match[2].to_i
+          myMessageCount = 0
+          raise RuntimeError.new("Invalid message index #{messageIndex}") if messageIndex < 0 || messageIndex > MESSAGE_LOOKUP_COUNT
+          if (channel = lookup_channel(match[1]))
+            channel.history(MESSAGE_LOOKUP_COUNT).each do |message|
+              myMessageCount += 1 if message.author.id == @bot.profile.id
+              if myMessageCount > messageIndex
+                puts "Deleting: #{message.content}"
+                channel.delete_message(message.id)
+                return
+              end
             end
           end
+        end
+
+      when MSGRE
+        if (match = command.match(MSGRE))
+          if (channel = lookup_channel(match[1]))
+            @bot.send_message(channel.id, match[2])
+          end
+        end
+
+      end
+    end # run_command
+
+    def lookup_channel(name)
+      @bot.servers.each_value do |server|
+        # puts "checking server #{server} for ##{name}"
+        if (channel = server.text_channels.detect { |channel| channel.name == name })
+          return channel
         end
       end
     end
